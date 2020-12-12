d007ca29b9937f081bdd4a44fe27a3b9931e670d
Barrett <rossbarr@msu.edu>
2020-12-12 13:01:26 -0500

    Added flappy bird. Need to do assignment and mouse click.

---
+++ flappy-bird/bird.lua
@@ -0,0 +1,39 @@
+Bird = Class{}
+
+local GRAVITY = 20
+
+function Bird:init()
+	self.image = love.graphics.newImage('bird.png')
+	self.width = self.image:getWidth()
+	self.height = self.image:getHeight()
+	
+	self.x = VIRTUAL_WIDTH / 3 - (self.width / 2)
+	self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+	
+	self.dy = 0
+end
+
+function Bird:collides(pipe)
+	if self.x + self.width - 2 >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+		if self.y + self.height - 2 >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+			return true
+		end
+	end
+	
+	return false
+end
+
+function Bird:update(dt)
+	self.dy = self.dy + GRAVITY * dt
+	
+	if love.keyboard.keysPressed['space'] then
+		self.dy = -5
+		sounds['jump']:play()
+	end
+	
+	self.y = self.y + self.dy
+end
+
+function Bird:render()
+	love.graphics.draw(self.image, self.x, self.y)
+end
---
+++ flappy-bird/main.lua
@@ -0,0 +1,109 @@
+push = require "push"
+Class = require "class"
+
+require "bird"
+require "pipe"
+require "pipePair"
+
+require "StateMachine"
+require "states/BaseState"
+require "states/PlayState"
+require "states/TitleScreenState"
+require "states/ScoreState"
+require "states/CountdownState"
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+local BACKGROUND_SCROLL_SPEED = 30
+local BACKGROUND_LOOPING_POINT = 413
+
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+local GROUND_SCROLL_SPEED = 60
+
+function love.load()
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+	love.window.setTitle('Fifty Bird')
+	
+	smallFont = love.graphics.newFont('font.ttf', 8)
+	mediumFont = love.graphics.newFont('flappy.ttf', 14)
+	flappyFont = love.graphics.newFont('flappy.ttf', 28)
+	hugeFont = love.graphics.newFont('flappy.ttf', 56)
+	love.graphics.setFont(flappyFont)
+	
+	math.randomseed(os.time())
+	
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+		vsync = true,
+		fullscreen = false,
+		resizable = true
+	})
+	
+	gStateMachine = StateMachine {
+		['title'] = function() return TitleScreenState() end,
+		['play'] = function() return PlayState() end,
+		['score'] = function() return ScoreState() end,
+		['countdown'] = function() return CountdownState() end
+	}
+	gStateMachine:change('title')
+	
+	sounds = {
+		['jump'] = love.audio.newSource('jump.wav', 'static'),
+		['explosion'] = love.audio.newSource('explosion.wav', 'static'),
+		['hurt'] = love.audio.newSource('hurt.wav', 'static'),
+		['score'] = love.audio.newSource('score.wav', 'static'),
+		['music'] = love.audio.newSource('marios_way.mp3', 'static')
+	}
+	
+	sounds['music']:setLooping(true)
+	sounds['music']:play()
+
+	love.keyboard.keysPressed = {}
+end
+
+function love.update(dt)
+	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+	
+	gStateMachine:update(dt)
+	
+	love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+	push:start()
+	
+	love.graphics.draw(background, -backgroundScroll, 0)
+	
+	gStateMachine:render()
+		
+	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+		
+	push:finish()
+end
+
+function love.keypressed(key)
+	love.keyboard.keysPressed[key] = true
+	
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
+
+function love.keyboard.wasPressed(key)
+	if love.keyboard.keysPressed[key] then
+		return true
+	else
+		return false
+	end
+end
+
+function love.resize(w, h)
+	push:resize(w, h)
+end
